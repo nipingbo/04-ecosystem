699d192cf40bc830ee079bcffcf9e3aa0dae1627
Raymond Ni <raymond.ni@sap.com>
2024-05-21 09:23:14 +0800

    feature: introduce tracing & tracing-subsciber

--- examples/err.rs
+++ examples/err.rs
@@ -1,6 +1,30 @@
 use anyhow::Context;
-use ecosystem::MyError;
 use std::{fs, mem::size_of, num::ParseIntError};
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum MyError {
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Parse error: {0}")]
+    Parse(#[from] std::num::ParseIntError),
+    #[error("Serialze json error: {0}")]
+    SerializeJson(#[from] serde_json::Error),
+    #[error("Error: {0:?}")]
+    BigError(Box<BigError>),
+    #[error("Custom error: {0}")]
+    Custom(String),
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct BigError {
+    a: String,
+    b: Vec<String>,
+    c: [u8; 64],
+    d: u64,
+}
 
 fn main() -> Result<(), anyhow::Error> {
     println!("size of anyhow::Error is {}", size_of::<anyhow::Error>());
@@ -15,13 +39,10 @@ fn main() -> Result<(), anyhow::Error> {
     );
     println!("size of string is {}", size_of::<String>());
     println!("size of MyError is {}", size_of::<MyError>());
-
     let filename = "non-existent-file.txt";
     let _fd =
         fs::File::open(filename).with_context(|| format!("Can not find file: {}", filename))?;
-
     fail_with_error()?;
-
     Ok(())
 }
 
